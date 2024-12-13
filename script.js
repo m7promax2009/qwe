59cd22e5af2d510020553e54c38798ee796d8b48
m7promax2009 <lev940437@gmail.com>
2024-12-13 23:51:37 +0500

    'sd'

---
+++ script.js
@@ -0,0 +1,11 @@
+let son1 = prompt('String typedagi malumotni kiriting');
+let son2 = prompt('Number typedagi sonni kiriting');
+let son3 = prompt('Boolean typedagi malumotni kiriting');
+
+
+alert('Hamma malumot togri kiritilganiga ishonchingiz komilmi')
+
+alert(`Siz kiritgan ${son1} sozi string typega tegishli \nSiz kiritgan ${son2} sozi number typega tegishli \nSiz kiritgan ${son3} sozi boolean typega tegishli`);
+
+
+
